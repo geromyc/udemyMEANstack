6db114ee8a8eab7635d6577f705b99493ade3742
Geromy Cunningham <geromycunningham@gmail.com>
2018-02-03 22:43:24 +0000

    finished section1 of udemy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # udemyMEANstack
+
+This repo contains the MEAN stack application that is build through Full Stack Traing's MEAN Stack course.
---
+++ webServersNodeExpress/MEAN/app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var routes = require('./api/routes');
+
+app.set('port', process.env.PORT);
+
+app.use(function(req, res, next) {
+   console.log(req.method, req.url);
+   next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended : false }));
+
+app.use('/api', routes);
+
+// app.get('/', function(req, res) {
+//     console.log("GET the hompage");
+//     res
+//         .status(200)
+//         .sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+// app.get('/json', function(req, res) {
+//     console.log("GET the json");
+//     res
+//         .status(200)
+//         .json( {"jsonData" : true} );
+// });
+
+// app.get('/file', function(req, res) {
+//     console.log("GET the json");
+//     res
+//         .status(200)
+//         .sendFile(path.join(__dirname, 'app.js'));
+// });
+
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+    console.log('Magic happens on port ' + app.get('port'));
+});
