917617848eae13e186a3c80b7205b82f31525cf8
Geromy Cunningham <geromycunningham@gmail.com>
2018-02-09 00:28:12 +0000

    section2 complete

---
+++ webServersNodeExpress/MEAN/api/data/dbconnection.js
@@ -0,0 +1,25 @@
+var MongoClient = require('mongodb').MongoClient;
+var dburl = 'mongodb://' + process.env.IP + ':27017/MEAN'
+
+var _connection = null;
+
+var open = function() {
+    MongoClient.connect(dburl, function (err, client) {
+        if (err) {
+            console.log("DB connection failed");
+            return;
+        }
+        _connection = client.db("meanhotel");
+        console.log("DB connection open", client);
+    });
+    // set _connection
+};
+
+var get = function() {
+    return _connection;
+};
+
+module.exports = {
+    open : open,
+    get : get
+};
