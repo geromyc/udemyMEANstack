4bb81dad2317ccd27d06562d471934b5d0a0d03c
Geromy Cunningham <geromycunningham@gmail.com>
2018-02-15 12:46:26 +0000

    section 4 complete

--- webServersNodeExpress/MEAN/app.js
+++ webServersNodeExpress/MEAN/app.js
@@ -6,17 +6,24 @@ var bodyParser = require('body-parser');
 
 var routes = require('./api/routes');
 
+// Define the port to run on
 app.set('port', process.env.PORT);
 
+// Add middleware to console log every request
 app.use(function(req, res, next) {
    console.log(req.method, req.url);
    next();
 });
 
+// Set static directory before defining routes
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
+// Enable parsing of posted forms
 app.use(bodyParser.urlencoded({ extended : false }));
+app.use(bodyParser.json());
 
+// Add some routing
 app.use('/api', routes);
 
 // app.get('/', function(req, res) {
@@ -40,6 +47,7 @@ app.use('/api', routes);
 //         .sendFile(path.join(__dirname, 'app.js'));
 // });
 
+// Listen for requests
 var server = app.listen(app.get('port'), function() {
     var port = server.address().port;
     console.log('Magic happens on port ' + app.get('port'));
